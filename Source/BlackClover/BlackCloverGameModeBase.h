75b5a1fd24cde7ee3c217c5f0bb62fa4dafe484d
Rushabh <rushabhchamle1996@gmail.com>
2023-05-16 08:37:31 -0700

    Base Project Files

---
+++ Source/BlackClover/BlackCloverGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BlackCloverGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLACKCLOVER_API ABlackCloverGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
